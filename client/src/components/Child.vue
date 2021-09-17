967169fd9c61ab447930360ade86ddf782c316bb
Tristan <tristanshawnjohnson@gmail.com>
2021-09-16 19:25:00 -0500

    Made a little effect on hover for list items style

--- client/src/components/Child.vue
+++ client/src/components/Child.vue
@@ -13,6 +13,24 @@ export default {
 
 </script>
 
-<style>
+<style scoped>
+
+  li {
+    width: 50%;
+    height: 20px;
+    background-color: rgb(157, 143, 143);
+    color: rgb(143, 28, 143);
+    list-style-type: circle;
+  }
+
+  li:hover {
+    width: 50%;
+    height: 20px;
+    background-color: rgb(157, 143, 143);
+    color: rgb(143, 28, 143);
+    cursor: pointer;
+    list-style-type: disc;
+  }
+
 
 </style>
